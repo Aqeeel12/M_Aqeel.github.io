6d1e8270922bad2249557d7f3bb54c83501c626e
Mohammed Aqeel Mohammed Maseeroor <aqeelmasroor741@gmail.com>
2025-12-27 22:07:22 +0800

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const loading = document.getElementById("loading-screen");
-  const main = document.getElementById("main-page");
-
-  setTimeout(() => {
-    loading.style.display = "none";
-    main.style.display = "block";
-  }, 1500);
-});
